874d6320c25929d7d138cb9a2f6a39d5760a835d
anelya <135839065+AnelyaKurbanova@users.noreply.github.com>
2025-04-28 21:29:59 +0500

    upd str2

--- back/server/server/urls.py
+++ back/server/server/urls.py
@@ -29,5 +29,6 @@ urlpatterns = [
     re_path(r'^$', RedirectView.as_view(url=reverse_lazy('api-root'), permanent=False)),
 ]
 
+
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
