d1259aa679945f843726a691b62d8519d1b1c1fd
anelya <135839065+AnelyaKurbanova@users.noreply.github.com>
2025-04-28 21:19:50 +0500

    upd str

--- back/server/server/urls.py
+++ back/server/server/urls.py
@@ -1,17 +1,21 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path, re_path, include, reverse_lazy
-from django.views.generic.base import RedirectView
-
+from django.urls import path, re_path, include
+from rest_framework.routers import DefaultRouter
 from users.views import UserLogIn, UserSignUpAPIView
+
+router = DefaultRouter()
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-user-login/', UserLogIn.as_view()),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    re_path(r'^$', RedirectView.as_view(url=reverse_lazy('api-root'), permanent=False)),
     path('sign-up/', UserSignUpAPIView.as_view(), name='user-sign-up'),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+
+
+    path('api/', include(router.urls)),
 
     path('api/users/', include('users.urls')),
     path('api/posts/', include('post.urls')),
@@ -21,6 +25,8 @@ urlpatterns = [
     path('api/friend-requests/', include('friends.urls')),
     path('post/', include('post.urls')),
 
+
+    re_path(r'^$', RedirectView.as_view(url=reverse_lazy('api-root'), permanent=False)),
 ]
 
 if settings.DEBUG:
